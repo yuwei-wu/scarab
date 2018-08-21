b658ee9a8907e947059ffe79deeeade0faf7ba90
Daniel Mox <dan.c.mox@gmail.com>
2018-08-20 23:02:45 -0400

    update teleop nodes
    update key_to_twist to continuously publish at 10Hz
    add node to publish duty_cycle messages at 10Hz

--- scarab_twist/src/key_to_twist.cpp
+++ scarab_twist/src/key_to_twist.cpp
@@ -7,72 +7,67 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    std::string output_topic("cmd_vel");
-    double v_speed, v_inc;
-    double w_speed, w_inc;
+  ros::init(argc, argv, "key_to_twist");
+  ros::NodeHandle nh, pnh("~");
+  ros::Publisher cmd_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10);
 
-    geometry_msgs::Twist twist;
-    ros::Publisher pub;
-    ros::NodeHandle *node;
+  double v_speed, v_inc, w_speed, w_inc;
+  pnh.param("vspeed", v_speed, 0.6);
+  pnh.param("wspeed", w_speed, 0.6);
+  pnh.param("vinc", v_inc, 0.08);
+  pnh.param("winc", w_inc, 0.05);
 
-    ros::init(argc, argv, "key_to_twist");
-    if (argc > 1) {
-      output_topic = argv[1];
+  initscr();
+  clear();
+  noecho();
+  cbreak();
+  timeout(50); // wait 50 ms
+  nodelay(stdscr, TRUE);
+
+  int row = 0;
+  mvprintw(row++, 0, "Publishing to %s", cmd_pub.getTopic().c_str());
+  mvprintw(row++, 0, "Use w, a, s, d to drive, space to halt, and q to quit");
+
+  int ch;
+  ros::Rate r(10);
+  geometry_msgs::Twist twist;
+  while (nh.ok()) {
+    row = 2;
+    mvprintw(row++, 0, "Angular: % 0.2f", twist.angular.z);
+    mvprintw(row++, 0, "Linear:  % 0.2f", twist.linear.x);
+    mvprintw(row++, 0, "");
+    refresh();
+
+    ch = getch();
+
+    if (ch != ERR) {
+      if (ch == 'w') {
+        twist.linear.x += v_inc;
+      } else if (ch == 's') {
+        twist.linear.x -= v_inc;
+      } else if (ch == 'a') {
+        twist.angular.z += w_inc;
+      } else if (ch == 'd') {
+        twist.angular.z -= w_inc;
+      } else if (ch == ' ') {
+        twist.angular.z = 0;
+        twist.linear.x = 0;
+      } else if (ch == 'q') {
+        twist.angular.z = 0;
+        twist.linear.x = 0;
+        cmd_pub.publish(twist);
+        ros::Duration(0.5).sleep();
+        break;
+      }
     }
+    twist.linear.x = min(max(twist.linear.x, -v_speed), v_speed);
+    twist.angular.z = min(max(twist.angular.z, -w_speed), w_speed);
 
-    ros::NodeHandle n("~");
-    node = &n;
+    cmd_pub.publish(twist);
+    ros::spinOnce();
+    r.sleep();
+  }
+  endwin();
 
-    node->param("vspeed", v_speed, 0.6);
-    node->param("wspeed", w_speed, 0.6);
-    node->param("vinc", v_inc, 0.08);
-    node->param("winc", w_inc, 0.05);
-    output_topic = ros::names::resolve(output_topic);
-    pub = n.advertise<geometry_msgs::Twist>(output_topic, 10);
-
-    initscr();
-    clear();
-    noecho();
-    cbreak();
-    timeout(50); // wait 50 ms
-    int ch;
-
-    int row = 0;
-    mvprintw(row++, 0, "Publishing to %s", output_topic.c_str());
-    mvprintw(row++, 0, "Use w, a, s, d to drive, space to halt, and q to quit");
-    while(node->ok()) {
-        row = 2;
-        mvprintw(row++, 0, "Angular: % 0.2f", twist.angular.z);
-        mvprintw(row++, 0, "Linear:  % 0.2f", twist.linear.x);
-        mvprintw(row++, 0, "");
-        refresh();
-        ch = getch();
-        if (ch == 'w') {
-            twist.linear.x += v_inc;
-        } else if (ch == 's') {
-            twist.linear.x -= v_inc;
-        } else if (ch == 'a') {
-            twist.angular.z += w_inc;
-        } else if (ch == 'd') {
-            twist.angular.z -= w_inc;
-        } else if (ch == ' ') {
-            twist.angular.z = 0;
-            twist.linear.x = 0;
-        } else if (ch == 'q') {
-            twist.angular.z = 0;
-            twist.linear.x = 0;
-            pub.publish(twist);
-            ros::Duration(0.5).sleep();
-            break;
-        } else if (ch == ERR) {
-            continue;
-        }
-        twist.linear.x = min(max(twist.linear.x, -v_speed), v_speed);
-        twist.angular.z = min(max(twist.angular.z, -w_speed), w_speed);
-
-        pub.publish(twist);
-        ros::spinOnce();
-    }
-    endwin();
-    return 0;
+  return 0;
 }
