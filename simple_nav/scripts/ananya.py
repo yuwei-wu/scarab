7bace4ed54be9570b16bb97bbc7793d731d3be3a
Wenxin Liu <wenxinl@seas.upenn.edu>
2019-06-21 09:59:16 -0400

    ananya's solution

--- simple_nav/scripts/ananya.py
+++ simple_nav/scripts/ananya.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-import rospy
+import rospy	
+import functools
 import geometry_msgs.msg
 import numpy as np
 import tf.transformations as tft
 import sensor_msgs.msg
-
+from std_msgs.msg import Float64
 
 class MoveToGoal(object):
     def __init__(self, name):
@@ -13,12 +14,17 @@ class MoveToGoal(object):
         rospy.Subscriber(name+"/pose", geometry_msgs.msg.PoseStamped, self.pose_cb)
 	rospy.Subscriber(name+"/scan_throttle",sensor_msgs.msg.LaserScan,self.laser_cb)
         self._cmd_pub = rospy.Publisher(name+"/cmd_vel", geometry_msgs.msg.Twist, queue_size=1)
+	#self._pub=rospy.Publisher
         self._goal_available = False
 	self._angle_b=0
 	self._obstacle = False
 	self._obstacle_p=0
+	self._t=0.0
+	self._first_o=True	
+	self._time_now=0.0
 	#self._temporary_goal=False
 	#self._t_goal=0
+	self._extreme=False
 
     def pose_cb(self, data):
         x = data.pose.position.x
@@ -29,7 +35,6 @@ class MoveToGoal(object):
                                 data.pose.orientation.w])
         eul = tft.euler_from_quaternion(quat)
         self._angle = eul[-1]
-
         if self._goal_available:
             cmd = geometry_msgs.msg.Twist()
 	    dist=np.sqrt((self._goal_x-x)*(self._goal_x-x)+(self._goal_y-y)*(self._goal_y-y))
@@ -41,33 +46,58 @@ class MoveToGoal(object):
 		rospy.loginfo("goal reached")
  		self._cmd_pub.publish(cmd)
 	    else:
-		angle_a=np.arctan2((self._goal_y-y),(self._goal_x-x))
-		self._angle_b=angle_a-self._angle
-		cmd.angular.z=np.clip(0.3*self._angle_b,-0.5,0.5)
-		#cmd.angular.z=0
-		if self._obstacle:
-		  #rospy.loginfo("this range is at %s with range %s",i,ranges[i])
-		  rospy.loginfo("avoiding obstacle")
-		  cmd.linear.x=0.2
-		  rospy.loginfo(cmd.linear.x)
-		  if self._obstacle_p==0: 
-		 	 self._angle_b=0.8/0.3
+	      angle_a=np.arctan2((self._goal_y-y),(self._goal_x-x))
+	      self._angle_b=angle_a-self._angle
+	      cmd.angular.z=np.clip(0.5*self._angle_b,-0.5,0.5)
+	      j=self._t+1
+	      k=self._t+3
+    	      #rospy.loginfo("time stamp %s", self._t)
+	      #rospy.loginfo(self._first_o)
+   	      #rospy.loginfo("time no %s", rospy.get_time())
+	      if self._extreme:
+		j=self._t+2  
+	      if self._obstacle and dist>0.4:
+		if cmd.linear.x>0 or abs(self._angle_b)<0.3:  			 
+		  #j=self._t+1
+		  #k=self._t+3
+		  #rospy.loginfo("time stamp %s", self._t)
+		  #rospy.loginfo(self._first_o)
+		  #rospy.loginfo("time no %s", rospy.get_time())
+		  while self._time_now<=j:
+		    rospy.loginfo("avoiding obstacle")
+		    #cmd.linear.x=0.2
+		    cmd.linear.x=0.0
+		    rospy.loginfo("time stamp %s", self._t)
+		    rospy.loginfo("time now %s", self._time_now)
+		    rospy.loginfo("linear %s", cmd.linear.x)
+		    if self._obstacle_p==1: 
+		 	 self._angle_b=0.5
 		  	 rospy.loginfo("turning left")
 
-	  	  else:	
-		    	self._angle_b=-0.8/0.3
+	  	    else:	
+		    	self._angle_b=-0.5
 		     	rospy.loginfo("turning right")
-		  cmd.angular.z=np.clip(0.3*self._angle_b,-0.4,0.4)
-	  	else:
-		  rospy.loginfo("no obstacle")
-		
-		  if abs(self._angle_b)<0.2:
+		    cmd.angular.z=self._angle_b
+		    rospy.loginfo("angular %s", cmd.angular.z)
+		    self._cmd_pub.publish(cmd)
+		    #rospy.loginfo("timestamp %s",self._t) 
+		  while self._time_now>j and self._time_now<=k:
+		    cmd.angular.z=0
+		    rospy.loginfo("travelling forward")
+		    cmd.linear.x=0.2
+		    self._cmd_pub.publish(cmd)	
+		else:
+		  rospy.loginfo("turning only")
+		  cmd.linear.x=0
+	      else:
+		rospy.loginfo("no obstacle")
+	        if abs(self._angle_b)<0.2:
 			rospy.loginfo("moving forward")
 			#cmd.linear.x=np.clip(0.2*dist,-0.5,0.5)
 			cmd.linear.x=0.2
-			cmd.angular.z=0
+			#cmd.angular.z=0
 			#cmd.linear.x=0
-		  else:
+	        else:
 			cmd.linear.x=0
 			rospy.loginfo("turning only")
 			rospy.loginfo(self._angle_b)
@@ -77,8 +107,7 @@ class MoveToGoal(object):
 
 
             self._cmd_pub.publish(cmd)
-            # rospy.loginfo("publishing vel_cmd: %s" % cmd)
-
+            # rospy.loginfo("publishing vel_cmd: %s" % cmd)  	
     def goal_reached(self, dist):
 	if dist<0.1:
 		return True
@@ -91,32 +120,40 @@ class MoveToGoal(object):
         rospy.loginfo("obtained goal! (%s, %s)", self._goal_x, self._goal_y)
         self._goal_available = True
 
-    def laser_cb(self, data):
-	ranges=np.array(data.ranges)
-	i=400
-	while i<=560:
-	  if ranges[i]<=0.8 and ranges[i]>0.02:
+    def laser_cb(self, data1):
+	ranges=np.array(data1.ranges)
+	i=360
+	self._time_now=data1.header.stamp.to_sec()
+	self._obstacle=False
+	self._extreme=False
+	if self._obstacle:
+	  self.first_o=False
+	else:
+	  self.first_o=True
+	while i<=600:
+	  if ranges[i]<=1 and ranges[i]>0.015:
 		self._obstacle=True
+		
 		if i<=480:
 			self._obstacle_p=1
 		else:
 			self._obstacle_p=0
+		if ranges[i]<=0.5:
+		  	self._extreme=True
+		#rospy.sleep(2)
 	  i+=1
-	'''if self._obstacle:
+	if self._obstacle:
 	  rospy.loginfo("obstacle detected")
+	  if self._first_o:
+	  	self._t=data1.header.stamp.to_sec()
+		rospy.loginfo("setting time stamp")
 	else:
 	  rospy.loginfo("obstacle not detected")
-
-	
-    def laser_cb(self, data)
-	ranges=np.array(data.ranges)
-	i=480
-	while i<=640:
-	if ranges[i]<=0.8:
-		self._temporary_goal=True
-		self_t_goal
-'''
-
+    
+    ''' def callback(self):
+	if self._obstacle:
+	 timer=rospy.Timer(rospy.Duration(2), functools.partial(self.laser_cb,data, oneshot=True)'''
+    
 def run(name):
     rospy.init_node('vel_from_pose')
     mtg = MoveToGoal(name)
