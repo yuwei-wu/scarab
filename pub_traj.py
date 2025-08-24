e114576408a4f614b06514b5ea6a561f47ec128e
yuwei-wu <yuweiwu20001@outlook.com>
2025-08-23 21:28:41 -0400

    init

--- pub_traj.py
+++ pub_traj.py
@@ -8,10 +8,10 @@ from geometry_msgs.msg import PoseStamped
 import tf.transformations as tf_trans
 from visualization_msgs.msg import Marker, MarkerArray
 from std_msgs.msg import ColorRGBA
+from scarab_msgs.msg import MoveActionGoal
 
 
-
-def load_trajectory(filepath):
+def load_trajectory(filepath, robot):
     with open(filepath, 'rb') as f:
 
         rospy.loginfo(f"Loading trajectory from {filepath}...")
@@ -22,11 +22,34 @@ def load_trajectory(filepath):
         # it is a list of points, and convert into a single list
 
         num_robot = len(file)
-        traj = file[0]
+
+        print(f"Number of robots in file: {num_robot}")
 
 
-        #down sample the trajectory to reduce the number of points
-        traj = traj[::40]  # Adjust the step size as needed
+        #print file type
+        print(f"File type: {type(file)}")
+        #it is a dict
+        print(f"File keys: {file.keys()}")
+
+        keys = list(file.keys())
+
+        if robot == "scarab41":
+            traj = file[keys[0]]
+            #traj[-1][0] = -0.2  # Set last x to -0.2
+        elif robot == "scarab42":
+            traj = file[keys[1]]
+        elif robot == "scarab46":
+            traj = file[keys[2]]
+        elif robot == "scarab45":
+            traj = file[keys[3]]
+
+
+
+        #down sample the trajectory to reduce the number of points, and keep the first and last points
+        # start = traj[0]
+        # end = traj[-1]
+        traj = traj[::10]
+
 
         return traj
 
@@ -76,11 +99,16 @@ def publish_trajectory_markers(pub_marker_array, points, frame_id="map"):
     pub_marker_array.publish(marker_array)
 
 
-def publish_trajectory(points, robot, interval=2.0):
 
-    pub_name = f"/{robot}/move_base_simple/goal"
+def publish_trajectory(points, robot, interval=2.0, simulation=True):
+    if simulation:
+        pub_name = f"/{robot}/move_base_simple/goal"
+        pub = rospy.Publisher(pub_name, PoseStamped, queue_size=10)
+    else:
+        pub_name = f"/{robot}/move_base/goal"
+        pub = rospy.Publisher(pub_name, MoveActionGoal, queue_size=10)
+
     frame_id = f"{robot}/map"
-    pub = rospy.Publisher(pub_name, PoseStamped, queue_size=10)
     marker_pub = rospy.Publisher(f"/{robot}/trajectory_markers", MarkerArray, queue_size=10)
 
     rospy.init_node('trajectory_publisher', anonymous=True)
@@ -88,21 +116,47 @@ def publish_trajectory(points, robot, interval=2.0):
 
     publish_trajectory_markers(marker_pub, points, frame_id=frame_id)
 
-    for point in points:
-        pose = PoseStamped()
-        pose.header.stamp = rospy.Time.now()
-        pose.header.frame_id = frame_id
+    for i, point in enumerate(points):
+        x, y = point
 
-        print(f"Publishing point: {point}")
+        if simulation:
+            pose = PoseStamped()
+            pose.header.stamp = rospy.Time.now()
+            pose.header.frame_id = frame_id
+            pose.pose.position.x = x
+            pose.pose.position.y = y
+            pose.pose.position.z = 0.0
 
-        # Assuming [x, y, theta]
-        x, y= point
-        pose.pose.position.x = x
-        pose.pose.position.y = y
-        pose.pose.position.z = 0.0
+            qx, qy, qz, qw = tf_trans.quaternion_from_euler(0, 0, 1.5708)  # 90 degree rotation
+            pose.pose.orientation.x = qx
+            pose.pose.orientation.y = qy
+            pose.pose.orientation.z = qz
+            pose.pose.orientation.w = qw
+            
 
-        rospy.loginfo(f"Publishing to {pub_name}: ({x:.2f}, {y:.2f}")
-        pub.publish(pose)
+
+            rospy.loginfo(f"[SIM] Publishing to {pub_name}: ({x:.2f}, {y:.2f})")
+            pub.publish(pose)
+        else:
+            goal = MoveActionGoal()
+            goal.header.stamp = rospy.Time.now()
+            goal.header.frame_id = frame_id
+            goal.goal_id.stamp = goal.header.stamp
+            goal.goal_id.id = f"goal_{i}"
+            goal.goal.target_pose.header = goal.header
+            goal.goal.target_pose.pose.position.x = x
+            goal.goal.target_pose.pose.position.y = y
+            goal.goal.target_pose.pose.position.z = 0.0
+            # set 90 degree rotation
+            qx, qy, qz, qw = tf_trans.quaternion_from_euler(0, 0, -1.5708)
+            goal.goal.target_pose.pose.orientation.x = qx
+            goal.goal.target_pose.pose.orientation.y = qy
+            goal.goal.target_pose.pose.orientation.z = qz
+            goal.goal.target_pose.pose.orientation.w = qw
+
+            rospy.loginfo(f"[REAL] Publishing to {pub_name}: ({x:.2f}, {y:.2f})")
+            pub.publish(goal)
+
         rate.sleep()
 
 
@@ -113,14 +167,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Replay robot trajectory from a specified start time.")
     parser.add_argument("--start-time", required=True, help="Start time in format YYYY-MM-DD HH:MM:SS")
     parser.add_argument("--robot", default="scarab40", help="Robot name (default: scarab40)")
-    parser.add_argument("--file", default="Real_World/RM1/robot_trajectories.pkl", help="Trajectory file path")
+    parser.add_argument("--file", default="Real_World/RM2/robot_trajectories.pkl", help="Trajectory file path")
 
     args = parser.parse_args()
 
     try:
-        traj = load_trajectory(args.file)
+        traj = load_trajectory(args.file, args.robot)
         print(f"Loaded trajectory with {len(traj)} points.")
         wait_until(args.start_time)
-        publish_trajectory(traj, robot=args.robot, interval=2.0)
+        publish_trajectory(traj, robot=args.robot, interval=1.0)
     except rospy.ROSInterruptException:
         pass
